18ffd7ee58bc54660467eaed3d78095cff9b97a3
nicolelimat <nicolelima.eu@gmail.com>
2023-11-03 15:47:50 -0300

    feature: Add Game Over menu | fix: Fix Pause Menu screen resolution

---
+++ Saci's Party/Assets/Script/GameOverMenuController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenuController : MonoBehaviour
+{
+    public GameObject gameOverMenu;
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+    }
+
+    public void PartyHall()
+    {
+        SceneManager.LoadScene("Party Hall");
+        Time.timeScale = 0;
+    }
+    
+}
--- Saci's Party/Assets/Script/MenuManager.cs
+++ Saci's Party/Assets/Script/MenuManager.cs
@@ -1,29 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public GameObject gameOverMenu;
+    public CanvasGroup gameOverCanvasGroup;
     bool isPaused;
+    bool isOver;
 
-    void Start(){
+    void Start()
+    {
         pauseMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        gameOverCanvasGroup.alpha = 0; // Painel de game over totalmente transparente
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (isOver == false && Input.GetKeyDown(KeyCode.Escape))
         {
             isPaused = pauseMenu.gameObject.activeSelf;
-            if(isPaused){
+            if (isPaused)
+            {
                 pauseMenu.SetActive(false);
                 Time.timeScale = 1;
-            } else {
+            }
+            else
+            {
                 pauseMenu.SetActive(true);
                 Time.timeScale = 0;
             }
         }
+    }
+
+    private void OnEnable()
+    {
+        SaciController.OnPlayerDeath += EnableGameOverMenu;
+    }
+
+    private void OnDisable()
+    {
+        SaciController.OnPlayerDeath -= EnableGameOverMenu;
+    }
+
+    public void EnableGameOverMenu()
+    {
+        gameOverMenu.SetActive(true);
+
+        // Efeito de fade-in gradual
+        StartCoroutine(FadeInGameOverPanel());
+    }
+
+    IEnumerator FadeInGameOverPanel()
+    {
+        float duration = 0.5f; // Duração do fade 
+        float timer = 0;
+
+        while (timer < duration)
+        {
+            float alpha = Mathf.Lerp(0, 1, timer / duration);
+            gameOverCanvasGroup.alpha = alpha;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        // Painel de game over totalmente visível no final
+        gameOverCanvasGroup.alpha = 1;
     }
 }
