1f4442f9fce4eb64215916db5e8b1891497822f7
nicolelimat <nicolelima.eu@gmail.com>
2023-11-14 02:11:31 -0300

    feat: Add level soundtrack

--- Saci's Party/Assets/Script/AudioController.cs
+++ Saci's Party/Assets/Script/AudioController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AudioController : MonoBehaviour
 {
-    AudioSource audioSource;
+    public static AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
