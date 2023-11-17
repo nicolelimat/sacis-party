80fcc141617f97dc050032bbb2fd8142c29373bc
nicolelimat <nicolelima.eu@gmail.com>
2023-11-16 21:25:41 -0300

    feat: Basic Party Hall (wo/ Portals)

--- Saci's Party/Assets/Script/SaciController.cs
+++ Saci's Party/Assets/Script/SaciController.cs
@@ -6,12 +6,12 @@ using UnityEngine.UI;
 
 public class SaciController : MonoBehaviour
 {
-    public float speed = 3.0f;
+    public float speed = 5.0f;
     public int health;
     public static int currentHealth;
     public float timeInvincible = 3.0f;
 
-    int maxHealth = 1;
+    int maxHealth = 6;
     bool isInvincible;
     float invincibleTimer;
     float horizontal; 
