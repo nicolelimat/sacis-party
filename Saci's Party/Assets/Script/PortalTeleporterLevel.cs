e5717671673d701f48f24affcc218f33d5868a1a
nicolelimat <nicolelima.eu@gmail.com>
2023-11-17 04:34:31 -0300

    feat: Portals

---
+++ Saci's Party/Assets/Script/PortalTeleporterLevel.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class PortalTeleporterLevel : MonoBehaviour
+{
+    public TextMeshProUGUI mensagemText;
+
+    void Start()
+    {
+        mensagemText.gameObject.SetActive(false);
+    }
+
+    IEnumerator ShowAndHideMessage(float delay)
+    {
+        mensagemText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(delay);
+        mensagemText.gameObject.SetActive(false);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject[] allEnemies = GameObject.FindGameObjectsWithTag("Enemy");
+            if (allEnemies.Length <= 0)
+            {
+                SceneManager.LoadScene("WinGame");
+            }
+            else
+            {
+                StartCoroutine(ShowAndHideMessage(2f)); // Exibe a mensagem por 2 segundos
+            }
+        }
+    }
+}
